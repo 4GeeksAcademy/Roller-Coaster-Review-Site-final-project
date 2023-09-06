7ae3c7890ec11900d94f79b18c1c9fffb3ed6dca
Sarahlebrun2 <130923257+Sarahlebrun2@users.noreply.github.com>
2023-09-06 18:37:16 +0000

    added to css

--- src/front/js/pages/coaster.js
+++ src/front/js/pages/coaster.js
@@ -1,21 +1,23 @@
 import React from "react";
-export default function Coaster(){
-    return(
-        <div className="text-auto">
+export default function Coaster() {
+    return (
+        <div className="container">
             <div className="top-container d-flex">
                 <div className="coaster-picture">
-                    <img src="https://picsum.photos/500/300?random=1"/> 
+                    <img id="img" src="https://picsum.photos/500/300?random=1" />
                 </div>
                 <div className="coaster-info">
                     <div className="coaster-info-title d-flex flex-column">
                         <h1>Millennium Force</h1>
-                        <span className="bg-primary rounded-circle p-1">9.4</span>
+                        <i id="star" class="fa-solid fa-star"> <span className=" p-1">9.4</span></i>
+                    </div>
+                    <div>
                         <p>Manufacturer: Intamin</p>
                     </div>
 
                 </div>
             </div>
-            <div className="bottom-container mx-auto w-75 bg-light mt-5 p-3">
+            <div className="bottom-container bg-light mt-5 p-3 ">
                 <div className="review-header d-flex justify-content-between border-bottom-0 mb-3">
                     <h1>Review</h1>
                     <button className="btn btn-success">write review</button>
@@ -23,7 +25,7 @@ export default function Coaster(){
                 <div className="review-content">
                     <div className="review-user d-flex justify-content-between">
                         <div className="d-flex">
-                            <img className="rounded-circle" height="50px" src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"/>
+                            <img className="rounded-circle" height="50px" src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" />
                             <h4 className="mt-1 mx-2">User 1</h4>
                         </div>
                         <div>
@@ -32,9 +34,9 @@ export default function Coaster(){
                     </div>
                     <div className="review-comment">
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
-molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
-numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
-optio, eaque rerum! Provident similique accusantium nemo autem.</p>
+                            molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
+                            numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
+                            optio, eaque rerum! Provident similique accusantium nemo autem.</p>
                     </div>
                     <div className="review-rating d-flex justify-content-end">
                         <p>72</p>
@@ -44,6 +46,5 @@ optio, eaque rerum! Provident similique accusantium nemo autem.</p>
                     </div>
                 </div>
             </div>
-        </div>
-    )
+    </div>)
 }
