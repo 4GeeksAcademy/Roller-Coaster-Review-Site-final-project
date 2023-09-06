acc3f99e00a08aa5b8a7bbfaff1e493f29c74858
KonnorThatcher <131403452+KonnorThatcher@users.noreply.github.com>
2023-09-06 20:19:25 +0000

    Able to add coasters to db via POST

--- src/api/routes.py
+++ src/api/routes.py
@@ -28,23 +28,26 @@ def get_all_parks():
 @api.route('/parks', methods=['POST'])
 def add_park():
     '''
+    Konnor's Note: I used ChatGPT to generate this JSON File. If you do it that way, make sure to check for any typos
+    and that it is formated correctly based on the below example.
+
     POST {
         "park": {
-            "name": string (e.g. "Kings Island"),
-            "location": string (e.g. "Mason, Ohio, USA"),
-            "year_opened": integer (e.g. 1972),
+            "name": "Kings Island",         <-- string
+            "location": "Mason, Ohio, USA", <-- string
+            "year_opened": 1972,            <-- string
             "coasters": [
                 {
-                  "name": string ("Adventure Express"),
-                  "ride_type": string ("Mine train coaster"),
-                  "height": string ("50 feet"),
-                  "track_length": string ("1,150 feet"),
-                  "max_speed": string ("35 mph"),
-                  "manufacturer": string ("Arrow Dynamics"),
-                  "year_opened": integer (1991),
-                  "inversions": integer (0),
-                  "tallest_drop": string ("42 feet"),
-                  "drop_angle": string ("50 degrees")
+                  "name": "Adventure Express",          <-- string
+                  "ride_type": "Mine train coaster",    <-- string
+                  "height": "50 feet",                  <-- string
+                  "track_length": "1,150 feet",         <-- string
+                  "max_speed": "35 mph",                <-- string
+                  "manufacturer": "Arrow Dynamics",     <-- string
+                  "year_opened": 1991,                  <-- integer
+                  "inversions": 0,                      <-- integer
+                  "tallest_drop": "42 feet",            <-- string
+                  "drop_angle": "50 degrees"            <-- string
                 },
                 ...
             ]
@@ -100,6 +103,54 @@ def get_all_coasters():
     return jsonify(
         coasters=[coaster.serialize() for coaster in coasters]
     ), 200
+
+@api.route('/coasters', methods=['POST'])
+def add_coaster_to_park():
+    '''
+    Konnor's Note: I used ChatGPT to generate this JSON File. If you do it that way, make sure to check for any typos
+    and that it is formated correctly based on the below example.
+
+    POST {
+        "roller_coaster": {
+            "name": "Ricochet",                             <-- string
+            "park_name": "Carowinds",                       <-- string
+            "location": "Charlotte, North Carolina, USA",   <-- string
+            "ride_type": "Steel wild mouse coaster",        <-- string
+            "height": "54 feet",                            <-- string
+            "track_length": "1,195 feet",                   <-- string
+            "max_speed": "28 mph",                          <-- string
+            "manufacturer": "Mack Rides",                   <-- string
+            "year_opened": 2002,                            <-- integer
+            "inversions": 0,                                <-- integer
+            "tallest_drop": "54 feet",                      <-- string
+            "drop_angle": "52 degrees",                     <-- string
+        }
+    }
+    '''
+    coaster = request.json["roller_coaster"]
+    #print(coaster)
+
+    park = Park.query.filter_by(name=coaster["park_name"], location=coaster["location"]).first()
+    if not park:
+        return jsonify("The park listed is not in our database. We can't add this coaster to our database."), 400
+    
+    db.session.merge(Coaster(
+        name=coaster["name"],
+            year_opened=coaster["year_opened"],
+            park_id=park.id,
+            ride_type=coaster["ride_type"],
+            manufacturer=coaster["manufacturer"],
+            track_length=coaster["track_length"],
+            height=coaster["height"],
+            tallest_drop=coaster["tallest_drop"],
+            drop_angle=coaster["drop_angle"],
+            max_speed=coaster["max_speed"],
+            inversions=coaster["inversions"]
+    ))
+
+    db.session.commit()
+
+    return '', 204
 
 @api.route('/coasters/<int:id>', methods=['GET'])
 def get_coaster(id):
