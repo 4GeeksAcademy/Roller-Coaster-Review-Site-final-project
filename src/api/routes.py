80a7835b217b0a03a63d135349eba2e9d4a1a52a
KonnorThatcher <131403452+KonnorThatcher@users.noreply.github.com>
2023-09-07 17:58:58 +0000

    Signup/Login API Routes written

--- src/api/routes.py
+++ src/api/routes.py
@@ -2,6 +2,7 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
 from api.models import db, User, Park, Coaster
 from api.utils import generate_sitemap, APIException
 
@@ -16,6 +17,51 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+@api.route('/signup', methods=['POST'])
+def signup():
+    '''
+    POST
+    {
+        "email": string,
+        "username": string,
+        "password": string
+    }
+    '''
+    data = request.json
+    print(data)
+    user = User.query.filter_by(email=data.get("email", None)).first()
+
+    if user:
+        return jsonify(message="This user already exists. Get outta here!"), 400
+    
+    user = User(
+        email=data["email"],
+        password=data["password"],
+        username=data["username"]
+    )
+    db.session.add(user)
+    db.session.commit()
+    return '', 204
+
+@api.route('/login', methods=['POST'])
+def login():
+    '''
+    POST
+    {
+        "email": string,
+        "password": string
+    }
+    '''
+    data = request.json
+    print(data)
+    user = User.query.filter_by(email=data.get("email", None)).first()
+
+    if not user or not user.check_password(data.get("password", None)):
+        return jsonify(message="Invalid Credentials"), 401
+
+    token = create_access_token(user.email)
+    return jsonify(token=token), 200
 
 @api.route('/parks', methods=['GET'])
 def get_all_parks():
