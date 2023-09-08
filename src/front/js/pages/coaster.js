85e77d57967c61c4c2b49843e184dd03ee78a8e2
Sarahlebrun2 <130923257+Sarahlebrun2@users.noreply.github.com>
2023-09-08 15:12:41 +0000

    update index.css

--- src/front/js/pages/coaster.js
+++ src/front/js/pages/coaster.js
@@ -1,8 +1,8 @@
 import React from "react";
 export default function Coaster() {
     return (
-        <div className="container">
-            <div className="top-container d-flex">
+        <div className="coaster-container">
+            <div className="top-container d-flex gap-3 justify-content-center">
                 <div className="coaster-picture">
                     <img id="img" src="https://picsum.photos/500/300?random=1" />
                 </div>
@@ -11,8 +11,26 @@ export default function Coaster() {
                         <h1>Millennium Force</h1>
                         <i id="star" class="fa-solid fa-star"> <span className=" p-1">9.4</span></i>
                     </div>
+                    <div className="coaster-locoation d-flex">
+                        <i class="fa-solid fa-location-dot"></i>
+                        <p>Cedar Point (Sandusky, Ohio, USA)</p>
+                    </div>
                     <div>
-                        <p>Manufacturer: Intamin</p>
+                        <ul className="general-info">
+                            <li>Manufacturer: Intamin</li>
+                            <li>Ride Type: Steel, Sit-down</li>
+                            <li>Year Opened: 2000</li>
+                        </ul>
+                    </div>
+                    <div>
+                        <ul className="coaster-stats">
+                            <li>Length: 6,595 ft</li>
+                            <li>Height/Drop: 310 ft / 300 ft</li>
+                            <li>Drop Angle: 80 degrees</li>
+                            <li>Max Speed: 93 mph</li>
+                            <li>Inversions: 0</li>
+                            <li>Ride Duration: 2:20</li>
+                        </ul>
                     </div>
 
                 </div>
@@ -23,26 +41,49 @@ export default function Coaster() {
                     <button className="btn btn-success">write review</button>
                 </div>
                 <div className="review-content">
-                    <div className="review-user d-flex justify-content-between">
-                        <div className="d-flex">
-                            <img className="rounded-circle" height="50px" src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" />
-                            <h4 className="mt-1 mx-2">User 1</h4>
+                    <div className="review">
+                        <div className="review-user d-flex justify-content-between">
+                            <div className="d-flex">
+                                <img className="rounded-circle" height="50px" src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" />
+                                <h4 className="mt-1 mx-2">User 1</h4>
+                            </div>
+                            <div>
+                            <i id="star" class="fa-solid fa-star"> <span className="rating-num">10</span> </i>
+                            </div>
                         </div>
-                        <div>
-                            <h4 className="rating-num">10</h4>
+                        <div className="review-comment">
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
+                                molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
+                                numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
+                                optio, eaque rerum! Provident similique accusantium nemo autem.</p>
                         </div>
-                    </div>
-                    <div className="review-comment">
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
-                            molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
-                            numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
-                            optio, eaque rerum! Provident similique accusantium nemo autem.</p>
-                    </div>
-                    <div className="review-rating d-flex justify-content-end">
-                        <p>72</p>
-                        <i className="fa-solid fa-thumbs-up mx-1"></i>
-                        <i className="fa-solid fa-thumbs-down mx-1"></i>
-                        <p>45</p>
+                        <div className="review-rating d-flex justify-content-end">
+                            <p>72</p>
+                            <i className="fa-solid fa-thumbs-up mx-1"></i>
+                            <i className="fa-solid fa-thumbs-down mx-1"></i>
+                            <p>45</p>
+                        </div>
+                        <div className="review-user d-flex justify-content-between">
+                            <div className="d-flex">
+                                <img className="rounded-circle" height="50px" src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" />
+                                <h4 className="mt-1 mx-2">User 1</h4>
+                            </div>
+                            <div>
+                            <i id="star" class="fa-solid fa-star"> <span className="rating-num">10</span> </i>
+                            </div>
+                        </div>
+                        <div className="review-comment">
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
+                                molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
+                                numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
+                                optio, eaque rerum! Provident similique accusantium nemo autem.</p>
+                        </div>
+                        <div className="review-rating d-flex justify-content-end">
+                            <p>72</p>
+                            <i className="fa-solid fa-thumbs-up mx-1"></i>
+                            <i className="fa-solid fa-thumbs-down mx-1"></i>
+                            <p>45</p>
+                        </div>
                     </div>
                 </div>
             </div>
