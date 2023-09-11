de03959ceb6e4818e7791889eb219949598f0d24
KonnorThatcher <131403452+KonnorThatcher@users.noreply.github.com>
2023-09-11 03:53:53 +0000

    Started work on integrating api to page

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -7,11 +7,11 @@ import { Home } from "./pages/home";
 import { Demo } from "./pages/demo";
 import { Single } from "./pages/single";
 import injectContext from "./store/appContext";
-import {UserProfile} from "./pages/userprofile";
+import { UserProfile } from "./pages/userprofile";
 
 import { Navbar } from "./component/navbar";
 import { Footer } from "./component/footer";
-import Review from "./pages/review";
+import CoasterReview from "./pages/coasterReviewPage";
 
 //create your first component
 const Layout = () => {
@@ -19,7 +19,7 @@ const Layout = () => {
     // you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
     const basename = process.env.BASENAME || "";
 
-    if(!process.env.BACKEND_URL || process.env.BACKEND_URL == "") return <BackendURL/ >;
+    if (!process.env.BACKEND_URL || process.env.BACKEND_URL == "") return <BackendURL />;
 
     return (
         <div>
@@ -29,7 +29,7 @@ const Layout = () => {
                     <Routes>
                         <Route element={<Home />} path="/" />
                         <Route element={<Demo />} path="/demo" />
-                        <Route element={<Review />} path="/review"/>
+                        <Route element={<CoasterReview />} path="/review/:coasterID" />
                         <Route element={<Single />} path="/single/:theid" />
                         <Route element={<h1>Not found!</h1>} />
                         <Route element={<UserProfile />} path="/UserProfile" />
