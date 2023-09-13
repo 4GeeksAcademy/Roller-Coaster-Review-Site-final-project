717bb0e048c09c4bbc293b07dafe6b69acd8d418
AlexisRosePolowsky <115315521+AlexisRosePolowsky@users.noreply.github.com>
2023-09-13 23:59:46 +0000

    finished signup & login & footer

---
+++ src/front/js/component/signup.js
@@ -0,0 +1,75 @@
+import React, { useState, useContext, useEffect } from "react";
+import { Context } from "../store/appContext";
+import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
+
+export const Signup = ({adjustFooterHeight}) => {
+  function declareHeight(){
+		adjustFooterHeight(true)
+	}
+
+	useEffect(declareHeight,[])
+  const { store, actions } = useContext(Context)
+  const [username, setUsername] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const navigate = useNavigate()
+  const token = sessionStorage.getItem("token")
+  //e is for event
+  useEffect(() => {
+    if (store.user != null) {
+      navigate("/")
+    }
+  })
+  const signup = (e) => {
+    e.preventDefault(
+      actions.createUser(username, email, password)
+    )
+  }
+
+  return (
+    <>
+      <form className="loginForm">
+        <div className="loginFormContent">
+          <h1>Create an Account</h1>
+          <div className="input-field">
+            <input
+              className="myInput"
+              type={"text"}
+              placeholder={"Username"}
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </div>
+          <div className="input-field">
+            <input
+              className="myInput"
+              type={"text"}
+              placeholder={"Email"}
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="input-field">
+            <input
+              className="myInput"
+              type={"password"}
+              placeholder={"Password"}
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <Link className="link" to="/login">
+            <span>Go Back</span>
+          </Link>
+          <br />
+        </div>
+        <div className="loginFormAction">
+          <button className="formBtn regBtn" onClick={(e) => signup(e)}>
+            Register
+          </button>
+        </div>
+      </form>
+    </>
+  );
+}
--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React,{useState,useEffect} from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ScrollToTop from "./component/scrollToTop";
 import { BackendURL } from "./component/backendURL";
@@ -12,12 +12,17 @@ import {UserProfile} from "./pages/userprofile";
 import { Navbar } from "./component/navbar";
 import { Footer } from "./component/footer";
 import Review from "./pages/review";
+import { Signup } from "./component/signup";
+import  Login  from "./component/login";
+
 
 //create your first component
 const Layout = () => {
     //the basename is used when your project is published in a subdirectory and not in the root of the domain
     // you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
     const basename = process.env.BASENAME || "";
+    const [fixFooter, setFixFooter] = useState(false)
+    
 
     if(!process.env.BACKEND_URL || process.env.BACKEND_URL == "") return <BackendURL/ >;
 
@@ -27,14 +32,16 @@ const Layout = () => {
                 <ScrollToTop>
                     <Navbar />
                     <Routes>
-                        <Route element={<Home />} path="/" />
+                        <Route element={<Home adjustFooterHeight={setFixFooter} />} path="/" />
                         <Route element={<Demo />} path="/demo" />
                         <Route element={<Review />} path="/review"/>
                         <Route element={<Single />} path="/single/:theid" />
                         <Route element={<h1>Not found!</h1>} />
-                        <Route element={<UserProfile />} path="/UserProfile" />
+                        <Route element={<UserProfile/>} path="/UserProfile" />
+                        <Route element={<Signup adjustFooterHeight={setFixFooter}/>} path="/signup"/>
+                        <Route element={<Login adjustFooterHeight={setFixFooter}/>} path="/login"/>
                     </Routes>
-                    <Footer />
+                    <Footer adjustHeight={fixFooter}/>
                 </ScrollToTop>
             </BrowserRouter>
         </div>
