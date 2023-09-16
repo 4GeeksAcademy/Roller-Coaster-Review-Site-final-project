71d110c52039fbb50611caef03830c0a7cd997c1
Austin Daniels <88160796+notaustindaniels@users.noreply.github.com>
2023-09-16 19:50:27 +0000

    Finished the park page I think

--- src/front/js/pages/park.js
+++ src/front/js/pages/park.js
@@ -3,15 +3,16 @@ import { Context } from "../store/appContext";
 
 export const Park = () => {
   return (
-      <div className="flex-container d-flex flex-column min-vh-100">  
+      <div className="flex-container d-flex flex-column min-vh-100 mb-4">  
           <div className="container mt-5">
             <div className="row">
               <div className="col-md-6">
-                <img src="/mnt/data/Screenshot 2023-09-15 at 8.05.47 PM.png" alt="The Park at OWA" className="img-fluid" />
+              <img id="img" src="https://picsum.photos/500/300?random=1" />
               </div>
-              <div className="col-md-6">
+              <div className="col-md-6 mt-5">
                 <h1>The Park at OWA</h1>
-                <p>Foley, Alabama, USA</p>
+                <p><i class="fa-solid fa-location-dot px-2 pt-1"></i>Foley, Alabama, USA</p>
+                <p>Year Opened: 2017</p>
                 <p>Roller Coasters: 3</p>
               </div>
             </div>
@@ -20,7 +21,7 @@ export const Park = () => {
             <table className="table">
               <thead>
                 <tr>
-                  <th>Name</th>
+                  <th>Roller Coasters</th>
                   <th>Rating</th>
                 </tr>
               </thead>
@@ -36,7 +37,12 @@ export const Park = () => {
               </tbody>
             </table>
           </div>
-          <div className="container mt-5">
+
+
+
+
+
+          <div className="reviews-div container mt-5 mb-5 pt-2 pb-2">
             <div className="d-flex justify-content-between align-items-center">
               <h3>Reviews</h3>
               <button className="btn btn-success">Write Review</button>
@@ -45,15 +51,16 @@ export const Park = () => {
             <div className="mt-4">
               <div className="d-flex justify-content-between align-items-center">
                 <div>
-                  <i className="fas fa-user"></i> <span>(Username Placeholder)</span>
+                  <i className="fas fa-user"></i> <span>User 1</span>
                 </div>
-                <div>(Rating Placeholder) <i className="fas fa-star"></i></div>
+                <div>7 <i className="fas fa-star"></i></div>
               </div>
               <p className="mt-3">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eu feugiat velit, sed condimentum nisi. Nulla facilisi.
               </p>
-              <div className="text-right">
-                <i className="fas fa-thumbs-up"></i> <i className="fas fa-thumbs-down"></i>
+              <div className="d-flex justify-content-end">
+                <p>32 <i className="fas fa-thumbs-up" style={{ marginRight: '5px' }}></i>
+                <i className="fas fa-thumbs-down"></i>19</p>
               </div>
             </div>
           </div>
